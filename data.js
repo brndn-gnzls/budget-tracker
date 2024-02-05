5dd9cb2d93ea5f0827857151d02af4f9cc7e9761
AE06909 <wes.gonzales@elevancehealth.com>
2024-02-05 08:21:59 -0500

    Code complete.

---
+++ data.js
@@ -0,0 +1,16 @@
+const transactions = [
+    {
+        id: 123,
+        source: "Freelancing",
+        amount: 499,
+        time: "ABC"
+    },
+    {
+        id: 123,
+        source: "Studio",
+        amount: "-49",
+        time: "ABC"
+    },
+    {},
+    {},
+]
